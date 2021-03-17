2728c7f1e5c61ef11efd44d8deee13c5919fcc74
Eeveebro279 <71796267+Eeveebro279@users.noreply.github.com>
2021-03-17 19:45:25 -0400

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,34 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1.2,
+            length: 350
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+
+            stroke("neon green");
+            strokeWeight(3);
+            line (pointB.x, pointB.y, pointA.x, pointA.y)
+            pop();
+        }
+    }
+
+} 
